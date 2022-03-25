b602f68229da768bd47c8e5f1df40362caddcb6b
UO277876 <79647031+UO277876@users.noreply.github.com>
2022-03-25 16:54:45 +0100

    SDI-2122-802-7.4-Recursos estáticos, peticiones POST y parámetros, Enrutamiento y comodines

--- musicstoreapp/routes/songs.js
+++ musicstoreapp/routes/songs.js
@@ -16,12 +16,19 @@ module.exports = function (app) {
     });
 
     app.get('/songs/:id', function(req, res) {
-        let response = 'id:' + req.params.id;
+        let response = 'id: ' + req.params.id;
         res.send(response);
     });
 
     app.get('/songs/:kind/:id', function(req, res) {
-        let response = 'id:' + req.params.id + '<br>' + 'Tipo de música:' + req.params.kind;
+        let response = 'id: ' + req.params.id + '<br>' + 'Tipo de música: ' + req.params.kind;
+        res.send(response);
+    });
+
+    app.post('/songs/add',function (req,res) {
+        let response = "Canción agregada: " + req.body.title + "<br>"
+        + " genero: " + req.body.kind + "<br>"
+        + " precio: " + req.body.price;
         res.send(response);
     });
 
